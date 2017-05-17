cdc780a85051c5b85569bb1b5e36d2b3c07a11a6
Vicki_G <vicki.l.greer@gmail.com>
2017-05-17 07:36:49 -0400

    Interim commit before upgrading WordPress to 4.7.5

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -18,11 +18,43 @@ get_header(); ?>
 		<div id="content" role="main">
 			
 			<?php while ( have_posts() ) : the_post(); ?>
+				<?php $services = get_field('services');
+					  $client = get_field('client');
+					  $link = get_field('site_link');
+					  $image1 = get_field('image_1');
+					  $image2 = get_field('image_2');
+					  $image3 = get_field('image_3');
+					  $size = "full";
+				?>
 				<article class="case-studies">
-					
-					<?php //the_title(); ?>
-					
-					<?php the_content(); ?>
+					<section class="case-studies-content">
+
+						<h2><?php the_title(); ?></h2>
+
+						<h4><?php echo $services; ?></h4>
+
+						<h5>Client: <?php echo $client ?></h5>
+						
+						<p><?php the_content(); ?></p>
+
+						<a href="<?php echo $site_link; ?>">Visit Live Site</a>
+
+					</section>
+
+					<section class="case-studies-images">
+						<?php if($image1) { 
+							echo wp_get_attachment_image( $image1, $size );
+						} ?>
+						<?php if($image2) { 
+							echo wp_get_attachment_image( $image2, $size );
+						} ?>	
+						<?php if($image3) { 
+							echo wp_get_attachment_image( $image3, $size );
+						} ?>
+
+
+					</section>
+
 				</article>
 			<?php endwhile; // end of the loop. ?>
 
